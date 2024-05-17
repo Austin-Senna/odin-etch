73642c807a876e20a949ca907d517e51e10b0156
Austin-Senna <austinsenna@gmail.com>
2024-05-17 22:27:29 +0700

    Finalise by creating grid function and buttons on top of it

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const container = document.querySelector(".container")
+let eraser = false
+const container = document.querySelector("#container")
+const resetButton = document.querySelector("#reset")
+const eraserButton = document.querySelector("#eraser")
+const drawButton = document.querySelector("#draw")
+const gridButton = document.querySelector("#new-grid")
 
-for (let i = 0; i<16; i++){
+function createGrid(n){
+for (let i = 0; i<n; i++){
     const row = document.createElement("div")
     row.classList.add("row")
-    for (let j = 0; j<16; j ++) {
+    for (let j = 0; j<n; j ++) {
         const col = document.createElement("div")
         col.classList.add("column")
         row.appendChild(col)
     }
     container.appendChild(row)
-}
+}}
 
-container.addEventListener("click", (x) =>  x.target.classList.add("columnH"))
+container.addEventListener("mouseover", (x) => {
+    if (x.target.className == "column" && eraser==false) {
+    x.target.classList.add("gray")}
+    else if (x.target.className =="column gray" && eraser == true) {
+    x.target.classList.remove("gray")
+    }
+})
+
+resetButton.addEventListener("click", () => {
+    const body = document.querySelectorAll(".gray")
+    body.forEach((x) => x.classList.remove("gray"))}
+)
+
+eraserButton.addEventListener("click", () => eraser = true)
+drawButton.addEventListener("click", () => eraser = false)
+gridButton.addEventListener("click", () => {
+    let newGridNumber = +prompt("For a x by x square, what x would you like?")
+    container.innerHTML = ""
+    createGrid(newGridNumber)
+})
+
+createGrid(16)
